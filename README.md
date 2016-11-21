cf6ee7b1e210af10a698a09632ed4f05a938c363
RobertPeek <robert@delta-net.co.uk>
2016-11-21 12:52:46 +0000

    Build

---
+++ README.md
@@ -0,0 +1,13 @@
+adapt-background-selector
+===============
+
+An extension that allows an image to be added to an article, block or component background.
+
+----------------------------
+**Version number:**  2.0.0     
+**Framework versions supported:**  2.0.4    
+**Author / maintainer:** DeltaNet with [contributors](https://github.com/deltanet/adapt-background-selector/graphs/contributors)     
+**Accessibility support:** Yes  
+**RTL support:** Yes
+**Authoring tool support:** yes
+----------------------------
---
+++ js/adapt-background-selector.js
@@ -0,0 +1,67 @@
+define(function(require) {
+
+var Adapt = require('coreJS/adapt');
+var Backbone = require('backbone');
+
+var BackgroundSelectorView = Backbone.View.extend({
+
+    initialize: function () {
+      if (this.model.get('_backgroundSelector') && this.model.get('_backgroundSelector')._isEnabled) {
+        this.render();
+        this.listenTo(Adapt, "remove", this.remove);
+        this.listenTo(Adapt, 'device:changed', this.setBackgroundImage);
+      }
+    },
+
+    events: {},
+
+    render: function () {
+
+      this.modelID = '.'+this.model.get('_id');
+      this.image = 'url('+this.model.get('_backgroundSelector')._src+')';
+      this.position = this.model.get('_backgroundSelector')._position;
+      this.size = this.model.get('_backgroundSelector')._size;
+      this.repeat = this.model.get('_backgroundSelector')._repeat;
+      this.attachment = this.model.get('_backgroundSelector')._attachment;
+      this.color = this.model.get('_backgroundSelector')._color;
+
+      this.setBackgroundImage();
+
+      return this;
+    },
+
+    setBackgroundImage: function () {
+      // Check device size
+      if (Adapt.device.screenSize === 'large' || Adapt.device.screenSize === 'medium') {
+        this.image = 'url('+this.model.get('_backgroundSelector')._src+')';
+      } else {
+        if(this.model.get('_backgroundSelector')._mobile._isEnabled){
+          this.image = 'url('+this.model.get('_backgroundSelector')._mobile._src+')';
+        }
+        if(this.model.get('_backgroundSelector')._hideOnMobile){
+          this.image = 'none';
+        }
+      }
+
+      $(this.modelID).css({
+          "background-image": this.image,
+          "background-position": this.position,
+          "background-size": this.size,
+          "background-repeat": this.repeat,
+          "background-attachment": this.attachment,
+          "background-color": this.color
+      });
+
+    }
+
+});
+
+Adapt.on('articleView:postRender blockView:postRender componentView:postRender', function(view) {
+    if (view.model.get("_backgroundSelector")) {
+        new BackgroundSelectorView({
+            model:view.model
+        });
+    }
+});
+
+});
